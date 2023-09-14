e923d081f0c8d42fefab879d0d6dffa80f6f1a10
Clovis910 <clovis910@hotmail.com>
2023-09-14 20:54:55 -0300

    multiplicacao

--- Exemplo.java
+++ Exemplo.java
@@ -22,10 +22,13 @@ public class Exemplo {
 
         int soma = numero1 + numero2;
 
-       int subtracao = numero1 - numero2;
+        int subtracao = numero1 - numero2;
+        int multiplicacao = numero1 * numero2;
+        
 
         System.out.println("Soma = " + soma);
          System.out.println("Subtracao= " + subtracao);
+         System.out.println("Multiplicacao= " + multiplicacao);
 
 
 
